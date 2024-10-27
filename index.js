a93417db68762f81cb8ef67c0353928048c7f83b
Spandana B Gowda <spandanabgowda0303@gmail.com>
2024-10-27 12:41:39 +0530

    feed:age updated

--- index.js
+++ index.js
@@ -5,10 +5,34 @@ const gender=document.getElementById('gender')
 const nameOutput = document.getElementById('nameOutput')
 const ageOutput= document.getElementById('ageOutput')
 const genderOutput=document.getElementById('genderOutput')
+const eligible=document.getElementById('eligible')
 
 driverFormEl.addEventListener("submit",(event)=>{
-    event.preventDefault()
+    event.preventDefault() //stop the reload of page
+    let userAge=calculateAge(age.value)
+    let eligibleToDrive = isEligibleToDrive(userAge)
     nameOutput.innerText=`Name: ${nameEl.value}`
-    ageOutput.innerText=`Age: ${age.value}`
+    ageOutput.innerText=`Age: ${userAge}`
     genderOutput.innerText=`Gender: ${gender.value}`
+    eligible.innerText=`Is Eligible to Drive: ${eligibleToDrive}`
+    console.log(eligibleToDrive)
 })
+
+function calculateAge(dob){
+    let today = new Date()
+    let dateofBirth = new Date(dob)
+    let age = today.getFullYear() - dateofBirth.getFullYear()
+    return age
+    
+
+}
+function isEligibleToDrive(age){
+    if(age>=18){
+        console.log('eligible')
+        return true
+    }
+    else{
+        console.log('not eligible')
+        return false
+    }
+}
